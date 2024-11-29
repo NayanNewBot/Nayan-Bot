8fe6845c02a787375d436e42da686e788a4174ac
NayanNewBot <kolushikder020@gmail.com>
2024-11-29 12:52:13 +0600

    Update npx.js

--- scripts/commands/npx.js
+++ scripts/commands/npx.js
@@ -22,7 +22,7 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
 				attachment: fs.createReadStream(__dirname + `/Nayan/Mayabi.mp3`)
 			}
 			api.sendMessage( msg, threadID, messageID);
-    api.setMessageReaction("😁", event.messageID, (err) => {}, true)
+    api.setMessageReaction("🙈", event.messageID, (err) => {}, true)
 		}
 	},
 	start: function({ nayan }) {
